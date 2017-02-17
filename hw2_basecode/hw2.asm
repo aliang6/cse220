08e7a737ff72ecca0fd7928a967c9f9f5d3c468b
Andy Liang <andy.liang.1@stonybrook.edu>
2017-02-17 16:10:32 -0500

    Homework 2 Added

---
+++ hw2_basecode/hw2.asm
@@ -0,0 +1,85 @@
+
+##############################################################
+# Homework #2
+# name: MY_NAME
+# sbuid: MY_SBU_ID
+##############################################################
+.text
+
+##############################
+# PART 1 FUNCTIONS
+##############################
+
+char2digit:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -200
+	############################################
+	jr $ra
+
+memchar2digit:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -200
+	############################################
+    jr $ra
+
+fromExcessk:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -200
+	li $v1, -200
+	############################################
+    jr $ra
+
+printNbitBinary:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -200
+	############################################
+    jr $ra
+
+##############################
+# PART 2 FUNCTIONS
+##############################
+
+btof:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -200
+	li $v1, -200
+	############################################
+    jr $ra
+
+print_parts:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -200
+	############################################
+    jr $ra
+
+print_binary_product:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -200
+	############################################
+    jr $ra
+
+
+
+#################################################################
+# Student defined data section
+#################################################################
+.data
+.align 2  # Align next items to word boundary
+
+#place all data declarations here
+
+
